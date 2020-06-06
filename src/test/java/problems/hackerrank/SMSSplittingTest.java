c70b65faca3b597e80da67c2d5592dc271dfe78f
Manish Wadhwani <manishwr@gmail.com>
2020-06-06 14:41:49 +0530

    Added hackerrank - SMS splitting problem

---
+++ src/test/java/problems/hackerrank/SMSSplittingTest.java
@@ -0,0 +1,26 @@
+package problems.hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SMSSplittingTest {
+    String problem;
+    List<String> result;
+
+    public void setupProblem1() {
+        problem = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis partu sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore ver rup. Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa.";
+        result = new ArrayList<>();
+        result.add("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis partu (1/3)"); 
+        result.add("sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore ver (2/3)");
+        result.add("rup. Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa. (3/3)");
+    }
+
+    @Test
+    public void testProblem1() {
+        setupProblem1();
+        Assert.assertArrayEquals(result.toArray(), SMSSplitting.segments(problem).toArray());
+    }
+}
