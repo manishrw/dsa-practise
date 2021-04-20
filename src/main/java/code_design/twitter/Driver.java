2a9bf2d9cd296195a252619f49dcfe72c0fb3016
Manish Wadhwani <manishwr@gmail.com>
2021-04-20 19:47:28 +0530

    Added code design -> Twitter problem with solution

---
+++ src/main/java/code_design/twitter/Driver.java
@@ -0,0 +1,15 @@
+package code_design.twitter;
+
+public class Driver {
+    public static void main(String[] args) {
+        Twitter twitter = new Twitter();
+        twitter.postTweet(1, 5);
+        System.out.println(twitter.getNewsFeed(1));
+        twitter.follow(1, 2);
+        twitter.postTweet(2, 6);
+        twitter.postTweet(2, 7);
+        System.out.println(twitter.getNewsFeed(1));
+        twitter.unfollow(1, 2);
+        System.out.println(twitter.getNewsFeed(1));
+    }
+}
