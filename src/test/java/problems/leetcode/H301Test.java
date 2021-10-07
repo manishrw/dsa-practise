bff0d67d23c7e8a7cab62cacbc9f058c24d75d68
manish wadhwani <manishrw@users.noreply.github.com>
2021-10-07 19:46:53 +0530

    Added Leetcode Hard 301 solution (#8)

---
+++ src/test/java/problems/leetcode/H301Test.java
@@ -0,0 +1,36 @@
+package problems.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class H301Test {
+
+    H301 solution;
+
+    public H301Test() {
+        this.solution = new H301();
+    }
+
+    @Test
+    void removeInvalidParentheses() {
+        String in = "()())()";
+        List<String> out = solution.removeInvalidParentheses(in);
+        assertEquals(2, out.size());
+        assertTrue(out.contains("(())()"));
+        assertTrue(out.contains("()()()"));
+
+        in = "(a)())()";
+        out = solution.removeInvalidParentheses(in);
+        assertEquals(2, out.size());
+        assertTrue(out.contains("(a())()"));
+        assertTrue(out.contains("(a)()()"));
+
+        in = ")(";
+        out = solution.removeInvalidParentheses(in);
+        assertEquals(   1, out.size());
+        assertEquals("", out.get(0));
+    }
+}
