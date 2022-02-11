693ab39203002c5c93070384484f4efcb873b320
Manish Wadhwani <mwadhwani@microsoft.com>
2022-02-11 10:57:23 +0530

    Added Leetcode M513 solution.

---
+++ src/main/java/problems/leetcode/M513.java
@@ -0,0 +1,60 @@
+/*
+* Medium 513 -  Find Bottom Left Tree Value
+*/
+package problems.leetcode;
+
+import java.util.*;
+
+import utils.Pair;
+
+public class M513 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        LinkedList<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int distance = 0, minDistance = 0, maxDistance = 0;
+        queue.add(new Pair<>(root, distance));
+        map.putIfAbsent(distance, root.val);
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Integer> pair = queue.removeFirst();
+            TreeNode node = pair.getKey();
+            distance = pair.getValue();
+            if (node.left != null) {
+                queue.add(new Pair<>(node.left, distance - 1));
+                map.putIfAbsent(distance - 1, node.left.val);
+            }
+            if (node.right != null) {
+                queue.add(new Pair<>(node.right, distance + 1));
+                map.putIfAbsent(distance + 1, node.right.val);
+            }
+            minDistance = Math.min(minDistance, distance);
+            maxDistance = Math.max(maxDistance, distance);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int key = minDistance; key <= maxDistance; key++) {
+            if (map.containsKey(key)) {
+                list.add(map.get(key));
+            }
+        }
+
+        return list.get(0);
+    }
+}
---
+++ src/test/java/problems/leetcode/M513Test.java
@@ -0,0 +1,29 @@
+package problems.leetcode;
+
+import org.junit.jupiter.api.Test;
+import problems.leetcode.M513.TreeNode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class M513Test {
+
+    private M513 solution = new M513();
+
+    @Test
+    void testPositiveCase() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        var result = solution.findBottomLeftValue(root);
+        assertEquals(4, result);
+    }
+
+    @Test
+    void testNullRoot() {
+        var result = solution.findBottomLeftValue(null);
+        assertEquals(0, result);
+    }
+}
