40357ac9626c4f7c6f3c4642cee82513f1d7fad5
Manish Wadhwani <mwadhwani@microsoft.com>
2022-11-30 16:06:36 +0530

    Added Leetcode Medium 79. Word Search

---
+++ src/main/java/dsa/problems/leetcode/M79.java
@@ -0,0 +1,62 @@
+/*
+ * Medium 79. Word Search
+ * https://leetcode.com/problems/word-search/
+ */
+package dsa.problems.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class M79 {
+    private static final boolean DEBUG_MODE = false;
+    private static final int[][] DELTA = new int[][] {
+            {1, 0},
+            {0, 1},
+            {-1, 0},
+            {0, -1}
+    };
+
+    public boolean exist(char[][] board, String word) {
+        StringBuilder sb = new StringBuilder(word.length());
+        Set<Integer> visited = new HashSet<>();
+        int n = board.length, m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (DEBUG_MODE) System.out.println("\nStarting from " + i + ", " + j);
+                if (exist(board, i, j, visited, word, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean exist(char[][] board, int x, int y, Set<Integer> visited, String word, int wordIdx) {
+        int n = board.length, m = board[0].length;
+        visited.add(x * m + y);
+        var subWord = word.substring(0, wordIdx + 1);
+        if (DEBUG_MODE) System.out.println("Substring word: " + subWord + " ; Idx: " + wordIdx + " ; Char: " + board[x][y]);
+
+        if (wordIdx == word.length() - 1) {
+            if (word.charAt(wordIdx) == board[x][y]) {
+                return true;
+            } else {
+                visited.remove(x * m + y);
+                return false;
+            }
+        }
+        if (word.charAt(wordIdx) != board[x][y]) {
+            visited.remove(x * m + y);
+            return false;
+        }
+
+        for (int[] ints : DELTA) {
+            int dx = ints[0], dy = ints[1];
+            int x1 = x + dx;
+            int y1 = y + dy;
+            if (x1 >= 0 && x1 < n && y1 >= 0 && y1 < m) System.out.println("-> " + x1 + ", " + y1);
+            if (x1 >= 0 && x1 < n && y1 >= 0 && y1 < m && !visited.contains(x1 * m + y1) && exist(board, x1, y1, visited, word, wordIdx + 1))
+                return true;
+        }
+        visited.remove(x * m + y);
+        return false;
+    }
+}
---
+++ src/test/java/dsa/problems/leetcode/M79Test.java
@@ -0,0 +1,36 @@
+package dsa.problems.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class M79Test {
+    M79 solution = new M79();
+
+    @Test
+    public void test1() {
+        char[][] board = {
+                {'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}
+        };
+        var word = "ABCCED";
+        assert solution.exist(board, word);
+    }
+    
+    @Test
+    public void test2() {
+        char[][] board = {
+            {'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}
+        };
+        var word = "ABCB";
+        assert !solution.exist(board, word);
+    }
+
+    @Test
+    public void test3() {
+        char[][] board = {
+                {'A','B'}
+        };
+        var word = "BA";
+        assert solution.exist(board, word);
+    }
+}
