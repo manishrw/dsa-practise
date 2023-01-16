915629615305f92817b5ea50c6553c9ebcd64962
Manish Wadhwani <mwadhwani@microsoft.com>
2023-01-16 16:48:58 +0530

    Find root of PerfectlyContainedBST

---
+++ src/main/java/dsa/problems/hackerrank/PerfectlyContainedBST.java
@@ -0,0 +1,51 @@
+/*
+ Problem: A list of BSTNode objects is called a perfectly contained BST if the following conditions are met:
+    1. The nodes in the list belong to one and only one BST.
+    2. All nodes in the list belong to the same BST.
+    3. All nodes in the BST belong to the list.
+
+    Implement the function findRoot, that given a list of BSTNode objects,
+    returns the root value of the BST if the list is perfectly contained, otherwise returns -1.
+ */
+package dsa.problems.hackerrank;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PerfectlyContainedBST {
+    public static int findRoot(List<List<Integer>> nodes) {
+        var nodeMap = new HashMap<Integer, List<Integer>>();
+        var parentMap = new HashMap<Integer, Integer>();
+        nodes.forEach(node -> {
+            var value = node.get(0);
+            var left = node.get(1);
+            var right = node.get(2);
+            if (left != -1) parentMap.put(left, value);
+            if (right != -1) parentMap.put(right, value);
+            nodeMap.put(value, node);
+        });
+
+        // Check all nodes are in the same tree
+        var roots = nodes.stream()
+            .map(node -> {
+                var value = node.get(0);
+                var left = node.get(1);
+                var right = node.get(2);
+                if (left != -1 && !nodeMap.containsKey(left) ||
+                    right != -1 && !nodeMap.containsKey(right)) {
+                    return -1;
+                }
+                return findRoot(value, parentMap);
+            }).collect(Collectors.toSet());
+
+        if (roots.contains(-1) || roots.size() != 1) return -1;
+        return roots.iterator().next();
+    }
+
+    private static int findRoot(int value, Map<Integer, Integer> parentMap) {
+        while (parentMap.containsKey(value)) {
+            value = parentMap.get(value);
+        }
+        return value;
+    }
+}
---
+++ src/test/java/dsa/problems/hackerrank/PerfectlyContainedBSTTest.java
@@ -0,0 +1,90 @@
+package dsa.problems.hackerrank;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class PerfectlyContainedBSTTest {
+    @Test
+    void testSingleRootPositive() {
+        List<List<Integer>> nodes = List.of(
+                List.of(17, -1, -1),
+                List.of(15, 13, 17),
+                List.of(7, -1, -1),
+                List.of(13, -1, -1),
+                List.of(5, 3, 7),
+                List.of(3, -1, -1),
+                List.of(10, 5, 15)
+        );
+
+        var expected = 10;
+        var actual = PerfectlyContainedBST.findRoot(nodes);
+        assert expected == actual;
+    }
+
+    @Test
+    void testSingleRootPositive2() {
+        List<List<Integer>> nodes = List.of(
+                List.of(48,30,10),
+                List.of(30,23,5),
+                List.of(23,-1,-1),
+                List.of(5,-1,-1),
+                List.of(10,-1,-1)
+        );
+
+        var expected = 48;
+        var actual = PerfectlyContainedBST.findRoot(nodes);
+        assert expected == actual;
+    }
+
+    @Test
+    void testSingleRootPositive3() {
+        List<List<Integer>> nodes = List.of(
+                List.of(48,30,10),
+                List.of(30,23,5),
+                List.of(23,-1,-1),
+                List.of(5,-1,-1),
+                List.of(10,-1,-1),
+                List.of(50,48,60),
+                List.of(60,-1,-1)
+        );
+
+        var expected = 50;
+        var actual = PerfectlyContainedBST.findRoot(nodes);
+        assert expected == actual;
+    }
+
+    @Test
+    void testInvalidBST() {
+        List<List<Integer>> nodes = List.of(
+                List.of(15, 13, 17),
+                List.of(7, -1, -1),
+                List.of(13, -1, -1),
+                List.of(5, 3, 7),
+                List.of(3, -1, -1),
+                List.of(10, 5, 15)
+        );
+
+        var expected = -1;
+        var actual = PerfectlyContainedBST.findRoot(nodes);
+        assert expected == actual;
+    }
+
+    @Test
+    void testMultipleRoots() {
+        List<List<Integer>> nodes = List.of(
+                List.of(30,23,5),
+                List.of(23,-1,-1),
+                List.of(5,-1,-1),
+                List.of(10,-1,-1),
+                List.of(50,48,60),
+                List.of(60,-1,-1),
+                List.of(70,50,80),
+                List.of(80,-1,-1)
+        );
+
+        var expected = -1;
+        var actual = PerfectlyContainedBST.findRoot(nodes);
+        assert expected == actual;
+    }
+}
